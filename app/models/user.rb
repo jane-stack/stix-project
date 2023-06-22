9dbda37c13449d4df527fb31c43e6f96db9ae4dd
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-22 17:11:33 -0600

    created controller for product_category

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
 
-    has_many :products
+    has_many :products, dependent: :destroy
     has_many :categories, through: :products
 
     validates :username, uniqueness: true, presence: true
