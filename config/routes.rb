09476524c5475a49d1ab154f185f244868a68924
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-22 17:20:03 -0600

    updated routes for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   resources :categories
   resources :products
   resources :users
+
+  # Users
+  post '/signup', to: 'users#create'
+  get '/me', to: 'useres#show'
+
+  # Sessions
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
