a7ed1015dab0c09149cfdb92e000c566f92459c1
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-22 17:16:04 -0600

    set up sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+    skip_before_action :authorize, only: [:create]
+
+    # POST /login
+    def create
+        user = User.find_by_username(params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: { errors: ["Username or Password didn't match"] }, status: :unprocessable_entity
+        end
+    end
+
+    # DELETE /logout
+    def destroy
+        session.delete :user_id
+        render json: { message: "You've Logged Out" }
+    end
 end
