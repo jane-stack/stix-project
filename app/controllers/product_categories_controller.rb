7b33c11e8a59c106df908a9aa95b7237ce65df7a
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-22 17:43:53 -0600

    set up product_category controller

--- app/controllers/product_categories_controller.rb
+++ app/controllers/product_categories_controller.rb
@@ -1,2 +1,16 @@
 class ProductCategoriesController < ApplicationController
+    def create
+        @product_category = ProductCategory.create(product_category_params)
+        if @product_category.valid?
+            render json: @product_category
+        else
+            render json: { errors: @product_category.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def product_category_params
+        params.permit(:product_id, :category_id)
+    end
 end
