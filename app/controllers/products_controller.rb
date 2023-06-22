4b2de838e9498988120e3f61be6b17b7bd753386
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-22 17:34:39 -0600

    set up product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+    def index
+        @product = Product.all
+        render json: @product
+    end
+
+    def show
+        @product = Product.find(params[:id])
+        @categories = @product.categories
+        render json: @categories
+    end
 end
