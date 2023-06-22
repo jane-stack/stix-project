c6e03c51252b57eec963f102556da50be189e26c
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-22 17:15:24 -0600

    set up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    before_action :authorize
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def current_user
+        User.find_by_id(session[:user_id])
+    end
+
+    def authorize
+        render json: { errors: ["You must be logged in"] }, status: :unauthorized unless logged_in?
+    end
+
+    def authorize_user_resource(user_id)
+        render json: { errors: ["You do not have permission for this resource"] }, status: :unauthorized unless user_id == current_user.id
+    end
 end
