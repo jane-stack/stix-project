9af751804e961e85c73187afd9de493568976481
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-22 17:20:40 -0600

    set up user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: [:create]
+
+    def index
+        render json: User.all
+    end
+
+    # POST /signup
+    def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            render json: user, status: 201
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    # GET /me
+    def show
+        render json: current_user
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password)
+    end
 end
