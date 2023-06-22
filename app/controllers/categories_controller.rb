0ee68c20a327b37b0b688105e5dbeaa7ba386494
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-22 17:37:47 -0600

    set up category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,12 @@
 class CategoriesController < ApplicationController
+    def index
+        @categories = Category.all
+        render json: @categories
+    end
+
+    def show
+        @category = Category.find(params[:id])
+        @product = @category.products
+        render json: @product
+    end
 end
